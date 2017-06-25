aa4e702b62042174a2572df91ce94d55f5c2eeaf
Isuru <isuru@venturit.com>
2017-06-25 15:15:12 +0530

    Added T9 mapping hash and modified matches

--- t9_words.rb
+++ t9_words.rb
@@ -5,7 +5,18 @@ class T9Words
     if File.exists?(dictionary_file)
       names = File.readlines(dictionary_file)
       # Testing number: 87825
-      puts names.select{|name| name[0].match(/[tuv]/) && name[1].match(/[pqrs]/) && name[2].match(/[tuv]/) && name[3].match(/[abc]/) && name[4].match(/[jkl]/) && name.strip.size == 5}.each{|w| w.strip!}
+      phone = "87825"
+      t9_map = {
+        2 => /[abc]/,
+        3 => /[def]/,
+        4 => /[ghi]/,
+        5 => /[jkl]/,
+        6 => /[mno]/,
+        7 => /[pqrs]/,
+        8 => /[tuv]/,
+        9 => /[wxyz]/
+      }
+      puts names.select{|name| name[0].match(t9_map[phone[0].to_i]) && name[1].match(t9_map[phone[1].to_i]) && name[2].match(t9_map[phone[2].to_i]) && name[3].match(t9_map[phone[3].to_i]) && name[4].match(t9_map[phone[4].to_i]) && name.strip.size == phone.size}.each{|w| w.strip!}
     else
       return "Error: Couldn't find the dictionary file."
     end
