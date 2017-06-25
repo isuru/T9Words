366c1cf3011f08527202f4bb04715e4c46e244f2
Isuru <isuru@venturit.com>
2017-06-25 14:59:26 +0530

    Matching a 5digit number and preparing file to directly test using command line (without irb)

--- t9_words.rb
+++ t9_words.rb
@@ -3,10 +3,13 @@ class T9Words
     dictionary_file = './data/dictionary.txt'
 
     if File.exists?(dictionary_file)
-      @names = File.readlines(dictionary_file)
-      return "#{@names.size} words found."
+      names = File.readlines(dictionary_file)
+      # Testing number: 87825
+      puts names.select{|name| name[0].match(/[tuv]/) && name[1].match(/[pqrs]/) && name[2].match(/[tuv]/) && name[3].match(/[abc]/) && name[4].match(/[jkl]/) && name.strip.size == 5}.each{|w| w.strip!}
     else
       return "Error: Couldn't find the dictionary file."
     end
   end
 end
+
+T9Words.search
